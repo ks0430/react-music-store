3a35a31ce3f64601d2ab8a4692b82bd0459f8bd1
Evelyn <ks0430@163.com>
2018-12-06 09:49:36 +1000

    Pass property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   styles = {
@@ -12,35 +12,36 @@ export default class Counter extends Component {
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log(this.props);
     let classes = this.getBadgeClasses();
 
     return (
-      <React.Fragment>
+      <div>
         <span className={classes}>{this.formatCount()}</span>
 
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement({})}
           className="btn btn-secondary"
           style={this.styles}
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
