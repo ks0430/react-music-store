6429bb0a5d81588ac14a84a9a9b1232f727c5b55
Evelyn <ks0430@163.com>
2018-12-06 13:53:49 +1000

    destructor {}

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          className="btn btn-primary btn-small m2"
-          onClick={this.props.onReset}
-        >
+        <button className="btn btn-primary btn-small m2" onClick={onReset}>
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id} // use internally by react
             counter={counter} //counter itself
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
           >
             <h5>Counter #{counter.id}</h5>
           </Counter>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// stateless functional component
+// sfc
+// Because it's stateless, so rerendered by parent component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
