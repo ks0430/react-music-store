f4635c1b7ad0b0a1eddb381bf96016a2b3b87333
Evelyn <ks0430@163.com>
2018-12-06 10:37:21 +1000

    Pass an event

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,11 +9,19 @@ class Counters extends Component {
       { id: 4, value: 3 }
     ]
   };
+
+  handleDelete = () => {
+    console.log("Event habdler called");
+  };
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          >
             <h4>Counter #{counter.id}</h4>
           </Counter>
         ))}
